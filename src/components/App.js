768d2644e4ac31883627d6ba9b073a525926be6f
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-26 12:02:18 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import ItemList from './ItemList';
 import ItemDetail from './ItemDetail';
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<ItemList />} />
         <Route path="/items/:id" element={<ItemDetail />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const items = [
-  { id: 1, name: 'Item 1', description: 'Description for Item 1' },
-  { id: 2, name: 'Item 2', description: 'Description for Item 2' },
-  { id: 3, name: 'Item 3', description: 'Description for Item 3' } 
-];
-
 function ItemDetail() {
   const { id } = useParams();
-  const item = items.find((item) => item.id === parseInt(id));
-
-  if (!item) return <p>Item not found</p>;
 
   return (
     <div>
-      <h1>{item.name}</h1>
-      <p>{item.description}</p>
+      <h1>Item {id}</h1>
+      <p>Description for Item {id}</p>
     </div>
   );
 }
